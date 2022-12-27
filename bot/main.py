9369110eabfe791ead8cc586b717d37d43485a83
Kletkin Vasilii <kletkinvasilii@gmail.com>
2022-12-27 22:11:32 +0400

    add bot

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,14 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+import os
+from dotenv import load_dotenv
 
-API_TOKEN = 'BOT TOKEN HERE'
+load_dotenv()
 
-# Configure logging
+API_TOKEN = os.getenv("API_TOKEN")
+
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
