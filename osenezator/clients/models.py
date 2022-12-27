35546c9a0140f7f393bcef03daffb72f31a8c466
Kletkin Vasilii <kletkinvasilii@gmail.com>
2022-12-27 21:38:21 +0400

    edit models

--- osenezator/clients/models.py
+++ osenezator/clients/models.py
@@ -21,6 +21,9 @@ class Client(models.Model):
     first_name = models.CharField("First Name", max_length=200,  blank=True)
     last_name = models.CharField("Last Name", max_length=200,  blank=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.address}"
