8707aa8270d17e5b4bdb98dd0253bf137893dea4
Kletkin Vasilii <kletkinvasilii@gmail.com>
2022-12-28 11:00:20 +0400

    add telegram driver

--- osenezator/drivers/models.py
+++ osenezator/drivers/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 
+class TelegramDriver(models.Model):
+    telegram_id = models.CharField("First Name", max_length=30)
+
+
 class Driver(models.Model):
     first_name = models.CharField("First Name", max_length=200, null=True)
     last_name = models.CharField("Last Name", max_length=200, null=True)
+    telegram_id = models.OneToOneField(
+        TelegramDriver, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = "Driver"
         verbose_name_plural = "Drivers"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
--- osenezator/users/models.py
+++ osenezator/users/models.py
@@ -1,2 +1,5 @@
 from django.db import models
 
+
+class User(BaseModel):
+    user=
