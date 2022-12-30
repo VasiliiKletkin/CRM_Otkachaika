ed6b8b1d4527b9434dcd3a888f726ac88a19f784
Kletkin Vasilii <kletkinvasilii@gmail.com>
2022-12-30 23:21:59 +0400

    add fix

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import logging
-from aiogram import Bot, Dispatcher, executor
-import handler
+from aiogram import Bot, Dispatcher, executor, types
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,5 +13,16 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """˝
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+    
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
