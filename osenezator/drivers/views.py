1116709a23394fac2385486644c4eb0063c44e80
Iulia Zagornova <zagornowwa.julia@gmail.com>
2022-12-30 22:42:03 +0400

    add paginacia

--- osenezator/drivers/views.py
+++ osenezator/drivers/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from .models import Driver
 from .serializers import DriverSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
 
 class DriverViewSet(viewsets.ModelViewSet):
     serializer_class = DriverSerializer
     queryset = Driver.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user_telegram_id',]
+    pagination_class = LimitOffsetPagination
--- osenezator/orders/views.py
+++ osenezator/orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Order
+from .serializers import OrderSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['driver',]
