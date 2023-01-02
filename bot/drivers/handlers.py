cc5fb48a90498f8fb6c14efd7d237e890c356dd2
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-01-02 20:18:51 +0400

    add handlers for driver

--- bot/drivers/handlers.py
+++ bot/drivers/handlers.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message, ReplyKeyboardRemove
-from .keyboards import main_menu_keyboard
+from aiogram.dispatcher.filters import Text
+from .keyboards import main_menu_keyboard, MAIN_MENU
 from config import dp
 
 
@@ -9,3 +10,10 @@ async def send_welcome(message: Message):
         "Вы можете смотреть все заказы, выполнять их и тд" \
         "Все функции указаны на клавиатуре"
     await message.answer(text, reply_markup=main_menu_keyboard())
+
+
+@dp.message_handler(Text(equals=MAIN_MENU.ORDERS, ignore_case=True))
+async def show_commands(message: Message):
+    await message.answer("Выбирете из следующего списка команд:", reply_markup=keyboard)
+    await OrderMessDriver.wait_choose_command.set()
+
