b6691cda7dcc662f0cf8c4b8554df317d065b831
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-01-16 11:47:27 +0400

    add fix

--- osenezator/profiles/admin.py
+++ osenezator/profiles/admin.py
@@ -13,4 +13,25 @@ class ProfileAdmin(admin.ModelAdmin):
             return qs
         return qs.filter(company=request.user.profile.company)
 
+    # def render_change_form(self, request, context, *args, **kwargs):
+    #     if request.user.is_superuser:
+    #         return super().render_change_form(request, context, *args, **kwargs)
+    #     context['adminform'].form.fields['user'].queryset = .objects.filter(company=request.user.profile.company)
+    #     return super().render_change_form(request, context, *args, **kwargs)
+
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        if request.user.is_superuser:
+            return fields
+        fields_to_remove = ('company', )
+        for field in fields_to_remove:
+            fields.remove(field)
+        return fields
+
+    def save_model(self, request, obj, form, change):
+        if request.user.is_superuser:
+            super().save_model(request, obj, form, change)
+        obj.company = request.user.profile.company
+        super().save_model(request, obj, form, change)
+
 admin.site.register(Profile, ProfileAdmin)
