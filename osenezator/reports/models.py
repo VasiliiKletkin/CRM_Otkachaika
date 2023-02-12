9d4cdc35b67af75ebf88f5b9beb360284a658602
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-02-12 23:33:59 +0400

    а

--- osenezator/reports/models.py
+++ osenezator/reports/models.py
@@ -4,6 +4,13 @@ from companies.models import Company
 
 class Report(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_summ  = models.DecimalField(max_digits=10, decimal_places=2)
+    count_orders = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Отчет'
+        verbose_name_plural = 'Отчеты'
 
     def __str__(self):
         return f'report:{self.id}, company:{self.company}'
