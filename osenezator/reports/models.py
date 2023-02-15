a33bcb5a314f42d3d12df1708b8551475d1477a1
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-02-15 21:44:16 +0400

    delete migrations

--- osenezator/reports/models.py
+++ osenezator/reports/models.py
@@ -10,7 +10,6 @@ class Report(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     date_start = models.DateField("Дата начала отчета")
     date_end = models.DateField("Дата конца отчета")
-    
     date_created = models.DateTimeField("Дата создания", auto_now_add=True)
     profit = models.DecimalField("Прибыль",max_digits=10, decimal_places=2, blank=True, null=True)
     count_orders = models.IntegerField("Колличество заказов", blank=True, null=True)
@@ -21,26 +20,32 @@ class Report(models.Model):
         verbose_name = 'Отчет'
         verbose_name_plural = 'Отчеты'
 
-    # def __str__(self):
-    #     return f'{self.id}, Date: {self.date.strftime("%m/%Y")}'
+    def __str__(self):
+        return f'{self.id}, Date: {self.date_start} - {self.date_end}'
 
-    # @property
-    # def orders(self):
-    #     return Order.objects.filter(company=self.company,
-    #                                 date_created__year=self.date.year,
-    #                                 date_created__month=self.date.month)
+    @property
+    def orders(self):
+        return Order.objects.filter(company=self.company,
+                                    date_created__gte=self.date_start,
+                                    date_created__lte=self.date_end)
 
-    # @property
-    # def drivers(self):
-    #     return Driver.objects.filter(profile__company=self.company,
-    #                                  orders__date_created__year=self.date.year,
-    #                                  orders__date_created__month=self.date.month
-    #                                  ).annotate(count_orders=Count('orders')
-    #                                             ).annotate(sum_orders=Sum('orders__price'))
+    @property
+    def drivers(self):
+        return Driver.objects.filter(profile__company=self.company,
+                                    date_created__gte=self.date_start,
+                                    date_created__lte=self.date_end
+                                     ).annotate(count_orders=Count('orders')
+                                                ).annotate(sum_orders=Sum('orders__price'))
 
-    # def calculate(self):
-    #     self.profit = self.orders.aggregate(Sum('price'))
-    #     self.count_orders = self.orders.count()
-    #     self.count_new_addresses = Address.objects.filter(company=self.company, date_created__year=self.date.year,
-    #                                                       date_created__month=self.date.month).count()
-    #     self.save()
+    def calculate(self):
+        self.profit = self.orders.aggregate(Sum('price'))
+        self.count_orders = self.orders.count()
+        self.count_new_addresses = Address.objects.filter(company=self.company,
+                                                        date_created__gte=self.date_start,
+                                                        date_created__lte=self.date_end
+                                                        ).count()
+        self.count_new_clients = Client.objects.filter(company=self.company,
+                                                        date_created__gte=self.date_start,
+                                                        date_created__lte=self.date_end
+                                                        ).count()
+        self.save()
