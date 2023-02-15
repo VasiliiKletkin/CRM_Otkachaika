eb2ddcb9f1c9999fcb492a49eb678ac29c00ed61
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-02-15 22:12:39 +0400

    add phobbe number

--- osenezator/companies/models.py
+++ osenezator/companies/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 from djmoney.models.fields import MoneyField
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Company(models.Model):
     name = models.CharField('Название компании', max_length=255)
-    phone_number = PhoneNumberField('Телефонный номер',)
+    phone_number = PhoneNumberField('Телефонный номер')
     city = models.CharField('Город', max_length=255, null=True, blank=True)
     country = models.CharField('Страна', max_length=255, null=True, blank=True)
     date_created = models.DateTimeField("Дата создания", auto_now_add=True)
-    balance = MoneyField('Баланс',max_digits=12, decimal_places=2, default_currency='RUB', default=0)
-    is_active = models.BooleanField('Активный',default=True)
+    balance = MoneyField('Баланс', max_digits=12,
+                         decimal_places=2, default_currency='RUB', default=0)
+    is_active = models.BooleanField('Активный', default=True)
 
     class Meta:
         verbose_name = "Компания"
--- osenezator/users/models.py
+++ osenezator/users/models.py
@@ -2,13 +2,14 @@ from companies.models import Company
 from django.contrib.auth.models import User
 from django.db import models
 from model_utils import Choices
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Profile(models.Model):
 
-    OWNER ='OWNER'
-    DRIVER ='DRIVER'
-    DISPATCHER ='DISPATCHER'
+    OWNER = 'OWNER'
+    DRIVER = 'DRIVER'
+    DISPATCHER = 'DISPATCHER'
 
     USER_TYPES = Choices(
         (OWNER, 'Владелец'),
@@ -18,4 +19,6 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.CharField(choices=USER_TYPES, max_length=20)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True) 
+    phone_number = PhoneNumberField('Телефонный номер')
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, null=True, blank=True)
