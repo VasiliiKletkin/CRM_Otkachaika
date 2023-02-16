4695cf741b981b2b5d6560891929281a20d262ab
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-02-16 23:58:58 +0400

    add new migrations

--- osenezator/orders/models.py
+++ osenezator/orders/models.py
@@ -1,33 +1,38 @@
+from django.utils import timezone
 from clients.models import Address
 from companies.models import Company
 from django.contrib.auth import get_user_model
 from django.db import models
 from model_utils import Choices
+from model_utils.fields import MonitorField, StatusField
 
 user_model = get_user_model()
 
 class Order(models.Model):
     CONFIRMATION = 'CONFIRMATION'
+    CONFIRMED = 'CONFIRMED'
     INPROGRESS = 'INPROGRESS'
     COMPLETED = 'COMPLETED'
     CANCELED = 'CANCELED'
 
-    ORDER_STATUSES = Choices(
+    STATUS = Choices(
         (CONFIRMATION, 'Подтверждение'),
+        (CONFIRMED, 'Подтвержден'),
         (INPROGRESS, 'Выполняется'),
         (COMPLETED, 'Выполнен'),
         (CANCELED, 'Отменен'),
     )
 
+    status = StatusField("Статус", default=CONFIRMED)
     driver = models.ForeignKey(user_model, on_delete=models.CASCADE, related_name='orders')
     address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='orders')
     dispatcher = models.ForeignKey(user_model, on_delete=models.CASCADE, related_name='orders_dispatcher')
     description = models.TextField("Описание", blank=True, null=True)
     price = models.DecimalField("Цена", max_digits=8, decimal_places=2)
     date_created = models.DateTimeField("Дата создания", auto_now_add=True)
-    date_completed = models.DateTimeField("Дата завершения", null=True, blank=True)
-    updated = models.DateTimeField("Последняя дата обновления", auto_now=True)
-    status = models.CharField("Статус", max_length=20, choices=ORDER_STATUSES, default=CONFIRMATION)
+    date_planned = models.DateTimeField("Планируемая дата выполнения", null=True, blank=True, default=timezone.now)
+    date_start = MonitorField("Дата начала выполнения", monitor='status', when=[INPROGRESS], null=True, blank=True, default=None, editable=False)
+    date_completed = MonitorField("Дата завершения", monitor='status', when=[COMPLETED], null=True, blank=True, default=None, editable=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='orders')
 
 
--- osenezator/orders/views.py
+++ osenezator/orders/views.py
@@ -1,10 +1,15 @@
 #from django.shortcuts import render
 
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.pagination import LimitOffsetPagination
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+
 from .models import Order
 from .serializers import OrderSerializer
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import LimitOffsetPagination
+
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.order_by('date_created')
@@ -12,4 +17,26 @@ class OrderViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['driver',]
     pagination_class = LimitOffsetPagination
+
+    @action(detail=True)
+    def canseled(self, request, pk=None):
+        order = get_object_or_404(Order, pk=pk)
+        order.status=Order.CANCELED
+        order.save()
+        return Response({'status': order.CANCELED})
+    
    
+    @action(detail=True)
+    def completed(self, request, pk=None):
+        order = get_object_or_404(Order, pk=pk)
+        order.status=Order.COMPLETED
+        order.save()
+        return Response({'status': order.status})
+    
+    @action(detail=True)
+    def canseled(self, request, pk=None):
+        order = get_object_or_404(Order, pk=pk)
+        order.status=Order.CANCELED
+        order.save()
+        return Response({'status': order.CANCELED})
+    
