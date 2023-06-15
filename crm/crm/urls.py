218db587ed6731883705dc9bc6ec651c7c3fe067
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-06-15 12:05:48 +0400

    add fix

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -19,13 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
-urlpatterns = [
-    path("sentry-debug/", trigger_error),
-    
+urlpatterns = [    
     path("addresses/", include("addresses.urls")),
     path("employees/", include("employees.urls")),
     path("users/", include("users.urls")),
